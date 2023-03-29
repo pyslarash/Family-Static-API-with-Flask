540a84b54ef34d693e6e8a2c26ce9d9d60884142
pyslarash <alex.pyslarash@gmail.com>
2023-03-29 02:15:44 +0000

    Finished

--- src/app.py
+++ src/app.py
@@ -25,18 +25,86 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
+@app.route('/test', methods=['GET'])
+def handle_test():
     response_body = {
-        "hello": "world",
-        "family": members
+        "message": "This is a test endpoint"
+    }
+    return jsonify(response_body), 200
+
+@app.route('/members', methods=['GET'])
+def handle_get_all_members():
+    # retrieve all family members from the jackson_family object
+    members = jackson_family.get_all_members()
+
+    # create a response body with the list of members
+    response_body = members
+
+    # return the response body as a JSON object with a 200 OK status code
+    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    # retrieve the member with the given ID from the jackson_family object
+    member = jackson_family.get_member(id)
+
+    # if no member with the given ID is found, return a 404 Not Found error
+    if not member:
+        response_body = {
+            "error": "Member not found"
+        }
+        return jsonify(response_body), 404
+
+    # create a response body with the member's information
+    response_body = {
+        "id": member["id"],
+        "first_name": member["first_name"],
+        "age": member["age"],
+        "lucky_numbers": member["lucky_numbers"]
     }
 
-
+    # return the response body as a JSON object with a 200 OK status code
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def handle_add_member():
+    # retrieve the request body as a JSON object
+    request_body = request.get_json()
+
+    # validate the request body and create a new member dictionary
+    new_member = {
+        "first_name": request_body.get("first_name"),
+        "age": request_body.get("age"),
+        "lucky_numbers": request_body.get("lucky_numbers"),
+        "id": request_body.get("id")
+    }
+
+    # add the new member to the jackson_family object
+    jackson_family.add_member(new_member)
+
+    # return an empty response with a 200 OK status code
+    return jsonify({}), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def handle_delete_member(member_id):
+    # delete the member with the given ID from the jackson_family object
+    deleted = jackson_family.delete_member(member_id)
+
+    # if no member with the given ID is found, return a 404 Not Found error
+    if not deleted:
+        response_body = {
+            "error": "Member not found"
+        }
+        return jsonify(response_body), 404
+
+    # create a response body with the "done" flag set to True
+    response_body = {
+        "done": True
+    }
+
+    # return the response body as a JSON object with a 200 OK status code
+    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,24 +13,88 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "id": self._generateId(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generateId(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # check if an ID is provided, otherwise generate a new one
+        if "id" not in member:
+            member["id"] = self._generateId()
+        else:
+            member_id = member["id"]
+            # check if the provided ID already exists
+            for m in self._members:
+                if m["id"] == member_id:
+                    raise APIException("Member with ID {} already exists".format(member_id), status_code=400)
+
+        # add the member to the list of members
+        self._members.append(member)
+
+        # return the new member's ID
+        return member["id"]
+
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        # loop through the list of members and delete the one with the given ID
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return True
+
+        # if no member with the given ID is found, return False
+        return False
+
+    def update_member(self, id, member):
+        # loop through the list of members and replace the one with the given ID
+        for i, m in enumerate(self._members):
+            if m["id"] == id:
+                member["id"] = id
+                self._members[i] = member
+                return True
+
+        # if no member with the given ID is found, return False
+        return False
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        # loop through the list of members and return the one with the given ID
+        for member in self._members:
+            if member["id"] == id:
+                return member
+
+        # if no member with the given ID is found, return None
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+    def test_get_single_member_implemented(client):
+        response = client.get('/member/3443')
+        print(response.status_code)
+        print(response.json())
+        assert response.status_code == 200
